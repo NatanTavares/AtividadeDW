98872658f14712c5c0588fcc9583cfa8d617f5de
NatanTavares <nattantavares.s15@gmail.com>
2020-06-22 22:42:24 -0300

    Test camera

--- view/app.js
+++ view/app.js
@@ -1,20 +1,25 @@
+const buttonOnCamera = document.querySelector('#buttonOnCamera');
 const buttonPanelRec = document.querySelector('#buttonPanelRec');
 const buttonPanelPic = document.querySelector('#buttonPanelPic');
 const buttonCancel = document.querySelector('#buttonCancel');
 const panelCamera = document.querySelector('.container');
 
-const camera = new CameraController();
+const videoCamera = document.querySelector('#video-camera');
+var camera;
+
+buttonOnCamera.addEventListener('click', event => {
+  togglePanel(true);
+  camera = new CameraController(videoCamera);
+});
 
 buttonPanelRec.addEventListener('click', event => {
   console.log("Rec");
-  togglePanel(true);
   // passr parametro para cameraController
   // Em um metodo tirarFotoOuGravar
 });
 
 buttonPanelPic.addEventListener('click', event => {
   console.log("Pic");
-  togglePanel(true);
   // passr parametro para cameraController
   // Em um metodo tirarFotoOuGravar
 });
@@ -31,10 +36,16 @@ panelCamera.addEventListener('click', event => {
 
 function togglePanel(turn) {
   if (turn) {
+    buttonOnCamera.classList.add('off');
     buttonCancel.classList.remove('off');
+    buttonPanelRec.classList.remove('off');
+    buttonPanelPic.classList.remove('off');
     panelCamera.classList.remove('off');
   } else {
+    buttonOnCamera.classList.remove('off');
     buttonCancel.classList.add('off');
+    buttonPanelRec.classList.add('off');
+    buttonPanelPic.classList.add('off');
     panelCamera.classList.add('off');
   }
 }
